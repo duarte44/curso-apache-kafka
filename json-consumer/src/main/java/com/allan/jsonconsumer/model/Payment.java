cf681d55ef70dc07651f2a6fbd7a1c81580dd0e1
Allan Duarte <allanantonucci47@gmail.com>
2023-02-23 14:52:56 -0300

    add json consumer

---
+++ json-consumer/src/main/java/com/allan/jsonconsumer/model/Payment.java
@@ -0,0 +1,21 @@
+package com.allan.jsonconsumer.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@ToString
+public class Payment implements Serializable {
+
+    private Long id;
+
+    private Long idUser;
+
+    private Long idProduct;
+
+    private String cardNumber;
+
+
+}
